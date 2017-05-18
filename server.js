b2e9d87c81fc3b0bc836a43e61ad9ccc4a4248b9
mutiara-tungga <putri@skyshi.com>
2017-05-19 05:59:07 +0700

    Refactor with Underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
-var bodyParser = require('body-parser'); //digunakan untuk mengambil data yang banyak(data json) yang di request (contohnya adalah yang digunakan di method POST)
+var bodyParser = require('body-parser'); //digunakan untuk mengambil data yang banyak(data json) dari body yang di request (contohnya adalah yang digunakan di method POST)
+var _ = require('underscore'); //dapat digunakan di array dan ... (undescorejs.org)
 
 var app = express();
 var PORT = 3000;
@@ -32,13 +33,15 @@ app.get('/todos', function (req, res) {
 
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10); //ini berupa string
-	var todoObject;
+	// var todoObject;
 
-	todos.forEach(function (todo){
-		if (todo.id === todoId) {
-			todoObject = todo;
-		}
-	});
+	// todos.forEach(function (todo){
+	// 	if (todo.id === todoId) {
+	// 		todoObject = todo;
+	// 	}
+	// });
+	/////kode di atas dapat di ringkas dengan modul underscore seperti di bawah ini
+	var todoObject = _.findWhere(todos, {id: todoId});
 
 	if (typeof todoObject === 'undefined') {
 		res.status(404).send();
@@ -46,11 +49,17 @@ app.get('/todos/:id', function (req, res) {
 		res.json(todoObject);
 	}
 
-	res.send('Asking for todo with id of '+ req.params.id)
+	res.send ('Asking for todo with id of '+ req.params.id)
 })
 
-app.post('/todos', function (req, res) {
-	var body = req.body;
+app.post ('/todos', function (req, res) {
+	var body = _.pick(req.body, 'description', 'completed');
+	//trim digunakan untuk menghilangkan spasi dari kata (di depan dan belakang kata) jika di tengah tidak akan hilang
+	if(! _.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send(); //400 digunakan untuk jika ada request yang tidak dapat diselesaikan
+	}
+
+	body.description = body.description.trim();
 	body.id = todoNextId++;
 
 	todos.push(body);
